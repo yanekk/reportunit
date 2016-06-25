9a8ef00d06ce743ca34fa96b5eca4a16201cc3ae
Janek Królikowski <yanekk86@gmail.com>
2016-06-25 14:11:08 +0200

    Added missing artifact files

---
+++ ReportUnit/Model/Artifact.cs
@@ -0,0 +1,13 @@
+﻿namespace ReportUnit.Model
+{
+    public class Artifact
+    {
+        public string FileName { get; private set; }
+        public string FilePath { get; set; }
+
+        public Artifact(string fileName)
+        {
+            FileName = fileName;
+        }
+    }
+}
---
+++ ReportUnit/Model/ArtifactSet.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+namespace ReportUnit.Model
+{
+    public class ArtifactSet
+    {
+        public readonly List<Artifact> Artifacts = new List<Artifact>();
+        public readonly string BasePath;
+
+        public ArtifactSet(string basePath)
+        {
+            BasePath = basePath;
+        }
+
+        public string DirectoryName
+        {
+            get { return Path.GetFileName(BasePath); }
+        }
+    }
+}
